752aa780114344846612b6c66399a5a779ba1827
Sean Norris <sn76@st-andrews.ac.uk>
2023-04-23 20:04:54 +0100

    Add matrix helper

---
+++ WordMatrixHelper/WordMatrixHelper.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace WordMatrixHelper
+{
+    partial class WordMatrixHelper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(364, 270);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.textBox_Focused);
+            this.textBox1.Enter += new System.EventHandler(this.textBox_Focused);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "I2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(382, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(364, 270);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Click += new System.EventHandler(this.textBox_Focused);
+            this.textBox2.Enter += new System.EventHandler(this.textBox_Focused);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(176, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "I3";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(217, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 30);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "I4";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(12, 335);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(734, 270);
+            this.outputTextBox.TabIndex = 0;
+            this.outputTextBox.Click += new System.EventHandler(this.textBox_Focused);
+            this.outputTextBox.Enter += new System.EventHandler(this.textBox_Focused);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(258, 299);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 30);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(299, 299);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(35, 30);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(340, 299);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(35, 30);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "X";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(381, 299);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(35, 30);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "inv";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 299);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(35, 30);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "B2";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(53, 299);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(35, 30);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "B3";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(94, 299);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(35, 30);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "B4";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(671, 299);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 30);
+            this.button11.TabIndex = 1;
+            this.button11.Text = "Round";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(594, 306);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(644, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "s.f.";
+            // 
+            // WordMatrixHelper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 617);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Name = "WordMatrixHelper";
+            this.Text = "WordMatrixHelper";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WordMatrixHelper/WordMatrixHelper.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace WordMatrixHelper
+{
+    public partial class WordMatrixHelper : Form
+    {
+        public WordMatrixHelper()
+        {
+            InitializeComponent();
+        }
+
+        private decimal[,] ParseMatrix(string wordFormat)
+        {
+            var raggedArray = wordFormat.Split('(').Last().Split(')').First().Split('@').Select(row => row.Split('&').Select(x => decimal.Parse(x)).ToArray()).ToArray();
+
+            var matrix = new decimal[raggedArray.Length, raggedArray[0].Length];
+            for (var y = 0; y < raggedArray.Length; y++)
+                for (var x = 0; x < raggedArray[y].Length; x++)
+                    matrix[y, x] = raggedArray[y][x];
+
+            return matrix;
+        }
+
+        private string EncodeMatrix(decimal[,] matrix)
+        {
+            var wordFormat = new StringBuilder();
+            wordFormat.Append("[■(");
+            for (var y = 0; y < matrix.GetLength(0); y++)
+            {
+                if (y > 0)
+                    wordFormat.Append('@');
+
+                for (var x = 0; x < matrix.GetLength(1); x++)
+                {
+                    if (x > 0)
+                        wordFormat.Append('&');
+
+                    wordFormat.Append(matrix[y,x]);
+                }
+            }
+
+            wordFormat.Append(")]");
+            return wordFormat.ToString();
+        }
+
+        private void textBox_Focused(object sender, EventArgs e)
+        {
+            if(!(sender is TextBox textBox) || string.IsNullOrEmpty(textBox.Text))
+                return;
+
+            textBox.SelectionStart = 0;
+            textBox.SelectionLength = textBox.TextLength;
+        }
+
+        private decimal[,] GenerateIdentityMatrix(int n)
+        {
+            var matrix = new decimal[n,n];
+
+            for (var y = 0; y < n; y++)
+                for (var x = 0; x < n; x++)
+                    matrix[y, x] = x == y ? 1 : 0;
+
+            return matrix;
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            outputTextBox.Text = EncodeMatrix(GenerateIdentityMatrix(2));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = EncodeMatrix(GenerateIdentityMatrix(3));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = EncodeMatrix(GenerateIdentityMatrix(4));
+        }
+
+        private string GenerateBlankMatrix(int n)
+        {
+            var wordFormat = new StringBuilder();
+            wordFormat.Append("[■(");
+            for (var y = 0; y < n; y++)
+            {
+                if (y > 0)
+                    wordFormat.Append('@');
+
+                for (var x = 1; x < n; x++)
+                    wordFormat.Append('&');
+            }
+
+            wordFormat.Append(")]");
+            return wordFormat.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = GenerateBlankMatrix(2);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = GenerateBlankMatrix(3);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = GenerateBlankMatrix(4);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var a = ParseMatrix(textBox1.Text);
+            var b = ParseMatrix(textBox2.Text);
+
+            var matrix = new decimal[a.GetLength(0), a.GetLength(1)];
+
+            for (var y = 0; y < a.GetLength(0); y++)
+                for (var x = 0; x < a.GetLength(1); x++)
+                    matrix[y, x] = a[y, x] + b[y, x];
+
+            outputTextBox.Text = EncodeMatrix(matrix);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var a = ParseMatrix(textBox1.Text);
+            var b = ParseMatrix(textBox2.Text);
+
+            var matrix = new decimal[a.GetLength(0), a.GetLength(1)];
+
+            for (var y = 0; y < a.GetLength(0); y++)
+                for (var x = 0; x < a.GetLength(1); x++)
+                    matrix[y, x] = a[y, x] - b[y, x];
+
+            outputTextBox.Text = EncodeMatrix(matrix);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var a = ParseMatrix(textBox1.Text);
+            var b = ParseMatrix(textBox2.Text);
+
+            var matrix = new decimal[a.GetLength(0), b.GetLength(1)];
+
+            for (var y = 0; y < a.GetLength(0); y++)
+            {
+                for (var x = 0; x < b.GetLength(1); x++)
+                {
+                    decimal val = 0;
+                    for (var i = 0; i < b.GetLength(0); i++)
+                        val += a[y, i] * b[i, x];
+
+                    matrix[y, x] = val;
+                }
+            }
+
+            outputTextBox.Text = EncodeMatrix(matrix);
+        }
+
+        decimal[,] FindInverse(decimal[,] matrix)
+        {
+            var identityMatrix = new decimal[matrix.GetLength(0), matrix.GetLength(1)];
+            for (var row = 0; row < identityMatrix.GetLength(0); row++)
+            {
+                for (var col = 0; col < identityMatrix.GetLength(1); col++)
+                {
+                    identityMatrix[row, col] = (row == col) ? 1 : 0;
+                }
+            }
+
+            var newMatrix = matrix.Clone() as decimal[,]; 
+
+            for (var outerRow = 0; outerRow < newMatrix.GetLength(0) - 1; outerRow++)
+            {
+                if (newMatrix[outerRow, outerRow] == 0)
+                {
+                    var swapWith = newMatrix.GetLength(0) - 1;
+                    while (newMatrix[outerRow, outerRow] == 0)
+                    {
+                        for (var col = 0; col < newMatrix.GetLength(1); col++)
+                        {
+                            var tempNewMatrix = newMatrix[outerRow, col];
+                            var tempIdentityMatrix = identityMatrix[outerRow, col];
+                            newMatrix[outerRow, col] = newMatrix[swapWith, col];
+                            identityMatrix[outerRow, col] = identityMatrix[swapWith, col];
+                            newMatrix[swapWith, col] = tempNewMatrix;
+                            identityMatrix[swapWith, col] = tempIdentityMatrix;
+                        }
+                        swapWith--;
+                    }
+                }
+                var divisor = newMatrix[outerRow, outerRow];
+                for (var col = 0; col < newMatrix.GetLength(1); col++)
+                {
+                    newMatrix[outerRow, col] /= divisor;
+                    identityMatrix[outerRow, col] /= divisor;
+                }
+
+                for (var innerRow = outerRow + 1; innerRow < newMatrix.GetLength(0); innerRow++)
+                {
+                    var multiplier = newMatrix[innerRow, outerRow];
+                    for (var col = 0; col < newMatrix.GetLength(1); col++)
+                    {
+                        newMatrix[innerRow, col] -= newMatrix[outerRow, col] * multiplier;
+                        identityMatrix[innerRow, col] -= identityMatrix[outerRow, col] * multiplier;
+                    }
+                }
+            }
+
+
+            var divisor2 = newMatrix[newMatrix.GetLength(0) - 1, newMatrix.GetLength(1) - 1];
+            for (var col = 0; col < newMatrix.GetLength(1); col++)
+            {
+                newMatrix[newMatrix.GetLength(0) - 1, col] /= divisor2;
+                identityMatrix[identityMatrix.GetLength(0) - 1, col] /= divisor2;
+            }
+
+            for (var outerRow = newMatrix.GetLength(0) - 2; outerRow >= 0; outerRow--)
+            {
+                for (var innerRow = newMatrix.GetLength(0) - 1; innerRow > outerRow; innerRow--)
+                {
+                    var multiplier = newMatrix[outerRow, innerRow];
+                    for (var col = 0; col < newMatrix.GetLength(1); col++)
+                    {
+                        newMatrix[outerRow, col] -= newMatrix[innerRow, col] * multiplier;
+                        identityMatrix[outerRow, col] -= identityMatrix[innerRow, col] * multiplier;
+                    }
+                }
+            }
+
+            return identityMatrix;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = EncodeMatrix(FindInverse(ParseMatrix(textBox1.Text)));
+        }
+
+        static decimal RoundToSignificantDigits(decimal val, int digits)
+        {
+            var truncateScale = (decimal) Math.Pow(10, digits + 3);
+            var d = Math.Truncate(val * truncateScale) / truncateScale;
+            if (d == 0)
+                return 0;
+
+            var scale = Math.Pow(10, Math.Floor(Math.Log10(Math.Abs((double)d))) + 1);
+            return (decimal)(scale * Math.Round((double)d / scale, digits));
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            var sigFigs = (int)numericUpDown1.Value;
+            var matrix = ParseMatrix(outputTextBox.Text);
+
+            for (var y = 0; y < matrix.GetLength(0); y++)
+                for (var x = 0; x < matrix.GetLength(1); x++)
+                    matrix[y, x] = RoundToSignificantDigits(matrix[y, x], sigFigs);
+
+            outputTextBox.Text = EncodeMatrix(matrix);
+        }
+    }
+}
